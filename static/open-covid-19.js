1e9afabea1c73ae31c61534dd582f81324eb73f5
owahltinez <41451293+owahltinez@users.noreply.github.com>
2020-03-22 07:29:27 +0000

    Deploying to gh-pages from  - cf87c383bce31475449acc3ba61ce88b56cf3ebb 🚀

--- static/open-covid-19.js
+++ static/open-covid-19.js
@@ -1,18 +1,22 @@
 /** Wait until all data is loaded, then perform callback */
 function loadData(callback) {
-    const data = { 'world': null, 'forecasting': null, 'charts': null };
-    const isReady = () => data['world'] && data['forecasting'] && data['charts'];
+    const data = { 'world': null, 'forecast': null, 'charts': null, 'history': null };
+    const isReady = () => data['world'] && data['forecast'] && data['charts'] && data['history'];
 
     $.getJSON(`${OPEN_COVID_19_URL}/data/data_latest.json`, world => {
         data['world'] = world;
         isReady() && callback(data);
     });
-    $.getJSON(`${OPEN_COVID_19_URL}/data/data_forecast.json`, forecasting => {
-        data['forecasting'] = forecasting;
+    $.getJSON(`${OPEN_COVID_19_URL}/data/data_forecast.json`, forecast => {
+        data['forecast'] = forecast;
         isReady() && callback(data);
     });
     $.getJSON(`${OPEN_COVID_19_URL}/data/charts/map.json`, charts => {
         data['charts'] = charts;
+        isReady() && callback(data);
+    });
+    $.getJSON(`${OPEN_COVID_19_URL}/data/data.json`, history => {
+        data['history'] = history;
         isReady() && callback(data);
     });
 }
