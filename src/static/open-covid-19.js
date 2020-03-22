622a008feddb83d339885187e21b3ea70d3794a6
Oscar Wahltinez <oscar@wahltinez.org>
2020-03-22 02:03:46 -0400

    Update data sources

--- src/static/open-covid-19.js
+++ src/static/open-covid-19.js
@@ -3,15 +3,15 @@ function loadData(callback) {
     const data = { 'world': null, 'forecasting': null, 'charts': null };
     const isReady = () => data['world'] && data['forecasting'] && data['charts'];
 
-    $.getJSON(`${OPEN_COVID_19_URL}/data/world_latest.json`, world => {
+    $.getJSON(`${OPEN_COVID_19_URL}/data/data_latest.json`, world => {
         data['world'] = world;
         isReady() && callback(data);
     });
-    $.getJSON(`${OPEN_COVID_19_URL}/forecasting/world.json`, forecasting => {
+    $.getJSON(`${OPEN_COVID_19_URL}/data/data_forecast.json`, forecasting => {
         data['forecasting'] = forecasting;
         isReady() && callback(data);
     });
-    $.getJSON(`${OPEN_COVID_19_URL}/forecasting/charts.json`, charts => {
+    $.getJSON(`${OPEN_COVID_19_URL}/data/charts/map.json`, charts => {
         data['charts'] = charts;
         isReady() && callback(data);
     });
