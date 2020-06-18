67215e0c80279d550b0740e39bf44389a6350cf0
owahltinez <41451293+owahltinez@users.noreply.github.com>
2020-06-18 22:13:18 +0000

    Deploying to gh-pages from  @ e63b991609bf321dc4c52f2fd0095621c3edc9ff 🚀

--- static/open-covid-19.js
+++ static/open-covid-19.js
@@ -17,3 +17,28 @@ function loadData(callback) {
     loadJSON('history', 'data.json');
     loadJSON('mobility', 'mobility.json');
 }
+
+function loadDataV2(callback) {
+    function loadJSON(key, path) {
+        $.getJSON(`${OPEN_COVID_19_URL}/data/v2/${path}`, json => {
+            callback(key, json);
+        });
+    }
+    Object.keys(OPEN_COVID_19_TABLES).forEach(
+        key => loadJSON(key, `${key}.json`));
+}
+
+
+function retryCallback(callback) {
+    let counter = 0;
+    const maxRetries = 32;
+    const timer = setInterval(() => {
+        try {
+            callback();
+            clearInterval(timer);
+        } catch (exc) {
+            console.error(`Callback failed: ${++counter}`, exc);
+            if (counter >= maxRetries) clearInterval(timer);
+        }
+    }, 250);
+}
